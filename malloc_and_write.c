5dca850c569b20885d4f83b38f1f472fe45d9601
Gary Little <gary.j.little@gmail.com>
2024-04-18 10:34:24 -0400

    Added malloc examples

---
+++ malloc_and_write.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    int bytes;
+    char *ptr,*ref;
+    if (argc == 2) {
+        printf("Allocating %s KB\n", argv[1]);
+        bytes=(atoi(argv[1])*1024);
+        printf("Allocating %d bytes\n",bytes);
+    } else {
+        printf("Please supply KB to allocate\n");
+        exit(1);
+    }
+    if (bytes>0) {
+        ptr=malloc(bytes);
+        printf("The address of your memory is %p\n",ptr);
+        for (ref=ptr;ref < (ptr+bytes);ref++) {
+            /*char is single quote, string is double quote*/
+            *ref='N';
+    }
+    }else{
+        printf("No memory allocated\n");
+    }
+    printf("Hit <return> to exit\n");
+    getchar();
+    printf("\n");
+}
