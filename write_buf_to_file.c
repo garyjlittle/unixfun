883727fb128dc023fe6353a2e7fc5371aefa30c2
Gary Little <gary.j.little@gmail.com>
2026-01-29 06:27:58 -0500

    Smaller

--- write_buf_to_file.c
+++ write_buf_to_file.c
@@ -7,14 +7,11 @@
 int main(int argc, char *argv[]) {
     int fd,bytes=8192;
     char *ptr,*ref;
-        printf("Allocating %d bytes\n",bytes);
         ptr=malloc(bytes);
-        printf("The address of your memory is %p\n",ptr);
         for (ref=ptr;ref < (ptr+bytes);ref++) {
             /*char is single quote, string is double quote*/
             *ref='N';
     }
-    printf("\n");
     fd=open("mybuff.dat",O_RDWR|O_CREAT,S_IRWXU);
     write(fd,ptr,bytes);
 }
